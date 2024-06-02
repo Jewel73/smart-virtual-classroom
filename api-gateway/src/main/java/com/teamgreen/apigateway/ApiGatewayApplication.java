6bd2a4d949e3471685310d55ed409a19477ba8c5
Jewel <jewelranar420@gmail.com>
2024-06-02 21:58:50 +0600

    update api gateway config

--- api-gateway/src/main/java/com/teamgreen/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/teamgreen/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
